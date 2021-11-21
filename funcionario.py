c8bc722f504c7f687004edfec1c1fa363f59991c
Filipe Leandro <lealves.work@gmail.com>
2021-11-21 17:57:11 -0300

    Add atualizações

--- funcionario.py
+++ funcionario.py
@@ -1,11 +1,16 @@
 from controle import incluir_filmes, listar_filmes, limpar
+from banco_dados import *
 
 def main():
     while True:
+        print('\n')
         print('\tMenu')
-        print('\n[1] - Cadastro de filme\n[2] - Listar filmes\n[0] - Sair\n')
+        print('\n[1] - Cadastro de filme\n[2] - Listar filmes\n[3] - Cadastrar sessões\n[0] - Sair\n')
 
         x = menu(int(input('>>> ')))
+
+        if x == False:
+            break
 
 def menu(x):
     limpar()
@@ -13,6 +18,8 @@ def menu(x):
         cadastro_filmes()
     elif x == 2:
         filmes_listados()
+    elif x == 3:
+        cadastrar_sessao()
     elif x == 0:
         return False
 
@@ -27,3 +34,22 @@ def cadastro_filmes():
 
 def filmes_listados():
     listar_filmes(1)
+
+def cadastrar_sessao():
+    file = open('bd_atuais.txt','r')
+    lista = file.readline()
+    lista = lista.split(';')
+
+    for i in range(len(lista)):
+        print(f'Filme nº{i + 1}: {lista[0]}')
+        sessoes.append(lista[0])
+    
+    while True:
+        escolha = (int(input("\nDeseja adicionar sessão para qual filme?\n>>> ")))
+
+        if escolha == 1:
+            sessao = input("\nDigite o número da sala e o horário da sessão...\n(Exemplo - Sala 1 - 10h30)\n\nDigite >>> ")
+            sessoes.append(sessao[escolha - 1][1])
+        break
+
+main()
--- teste.py
+++ teste.py
@@ -1,5 +1,12 @@
+from banco_dados import *
+
 print("Olá mundo")
 
 print("Teste branch")
 
 print('LUL')
+
+for i in range(len(filmes_breve)):
+    sessoes.append([filmes_breve[i][0]])
+
+print(sessoes)
